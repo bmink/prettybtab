86200743b40c070ce15e22801231205b7419d22e
Barna Mink <barnamink@gmail.com>
2019-05-23 11:27:16 -0700

    Initial functionality finished.

--- main.c
+++ main.c
@@ -7,12 +7,17 @@
 void usage(char *);
 
 #define PBT_STRINGCNT	4
+#define PBT_FRETNRWIDTH	3
+#define PBT_INITSPACER	2
+#define PBT_LINECHAR	'-'
+#define PBT_LINEWIDTH	78
 
+/* Note: we treat the E string as string[0]. */
 char *string_pref[PBT_STRINGCNT] = {
-	"G ---",
-	"D ---",
-	"A ---",
-	"E ---"
+	"E ",
+	"A ",
+	"D ",
+	"G "
 };
 
 
@@ -21,14 +26,19 @@ main(int argc, char **argv)
 {
 	int	err;
 	char	*execn;
+	bstr_t	*tempstr;
 	int	i;
-	bstr_t	*note;
-	barr_t	*notes;
+	note_t	*note;
+	bstr_t	*noteln;
+	bstr_t	*nstr;
+	barr_t	*nstrs;
 	int	ret;
 	bstr_t	*strings[PBT_STRINGCNT];
 
 	err = 0;
-	notes = NULL;
+	tempstr = NULL;
+	noteln = NULL;
+	nstrs = NULL;
 	for(i = 0; i < PBT_STRINGCNT; ++i) {
 		strings[i] = NULL;
 	}
@@ -53,9 +63,9 @@ main(int argc, char **argv)
 
 	if(argc == 1) {
 		/* No argument -- print an empty line. */
-		notes = barr_init(sizeof(bstr_t *));
+		nstrs = barr_init(sizeof(bstr_t *));
 	} else {
-		ret = xstrsplit(argv[1], ":", 0, &notes);
+		ret = xstrsplit(argv[1], ":", 0, &nstrs);
 		if(ret != 0) {
 			fprintf(stderr, "Couldn't parse notes: %s\n",
 			    strerror(ret));
@@ -64,13 +74,15 @@ main(int argc, char **argv)
 		}
 	}
 
-	printf("Got %d note(s).\n", barr_cnt(notes));
+#if 0
+	printf("Got %d note(s).\n", barr_cnt(nstrs));
 
-	for(note = (bstr_t *) barr_begin(notes);
-	    note < (bstr_t *) barr_end(notes);
-	    ++note) {
-		printf("(%d) %s\n", bstrlen(note), bget(note));
+	for(nstr = (bstr_t *) barr_begin(nstrs);
+	    nstr < (bstr_t *) barr_end(nstrs);
+	    ++nstr) {
+		printf("(%d) %s\n", bstrlen(nstr), bget(nstr));
 	}
+#endif
 
 	for(i = 0; i < PBT_STRINGCNT; ++i) {
 		strings[i] = binit();
@@ -81,29 +93,89 @@ main(int argc, char **argv)
 			goto end_label;
 		}
 		bstrcat(strings[i], string_pref[i]);
+		/* Initialize with one empty note's worth of chars */
+		bstrpad(strings[i], bstrlen(strings[i]) + PBT_INITSPACER, '-');
 	}
-	
 
-	for(note = (bstr_t *) barr_begin(notes);
-	    note < (bstr_t *) barr_end(notes);
-	    ++note) {
+	noteln = binit();
+	if(noteln == NULL) {
+		fprintf(stderr, "Couldn't allocate buffer for noteln\n");
+		err = ENOMEM;
+		goto end_label;
+	}
+	bstrpad(noteln, bstrlen(strings[0]), ' ');
+	
+	tempstr = binit();
+	if(tempstr == NULL) {
+		fprintf(stderr, "Couldn't allocate buffer for tempstr\n");
+		err = ENOMEM;
+		goto end_label;
+	}
+
+	for(nstr = (bstr_t *) barr_begin(nstrs);
+	    nstr < (bstr_t *) barr_end(nstrs);
+	    ++nstr) {
+		if(bstrlen(nstr) == 0) {
+			bclear(tempstr);
+			bstrpad(tempstr, PBT_FRETNRWIDTH, '-');
+		
+			for(i = PBT_STRINGCNT - 1; i >=0; --i) {
+				bstrcat(strings[i], bget(tempstr));
+			}
+
+			bclear(tempstr);
+			bstrpad(tempstr, PBT_FRETNRWIDTH, ' ');
+			bstrcat(noteln, bget(tempstr));
+			continue;
+		}
+
+		note = getnote(bget(nstr));
+		if(note == NULL) {
+			fprintf(stderr, "Invalid note specified: %s\n",
+			    bget(nstr));
+			err = ENOENT;
+			goto end_label;
+		}
+		for(i = PBT_STRINGCNT - 1; i >=0; --i) {
+			bclear(tempstr);
+			if(i == note->no_stringnr) {
+				bprintf(tempstr, "%d", note->no_fretpos);
+			}
+			bstrpad(tempstr, PBT_FRETNRWIDTH, '-');
+			bstrcat(strings[i], bget(tempstr));
+		}
+
+		bclear(tempstr);
+		for(i = 0; i < PBT_STRINGCNT; ++i) {
+			if(i == note->no_stringnr) {
+				bprintf(tempstr, "%s", note->no_name);
+				break;
+			}
+		}
+		bstrpad(tempstr, PBT_FRETNRWIDTH, ' ');
+		bstrcat(noteln, bget(tempstr));
 	}
 
 	for(i = 0; i < PBT_STRINGCNT; ++i) {
+		bstrpad(strings[i], PBT_LINEWIDTH, '-');
+	}
+	
+	printf("\n");
+	for(i = PBT_STRINGCNT - 1; i >=0; --i) {
 		printf("%s\n", bget(strings[i]));
 	}
-
+	printf("%s\n", bget(noteln));
+	printf("\n");
 	
 end_label:
 
-
-	if(notes != NULL) {
-		for(note = (bstr_t *) barr_begin(notes);
-		    note < (bstr_t *) barr_end(notes);
-		    ++note) {
-			buninit_(note);
+	if(nstrs != NULL) {
+		for(nstr = (bstr_t *) barr_begin(nstrs);
+		    nstr < (bstr_t *) barr_end(nstrs);
+		    ++nstr) {
+			buninit_(nstr);
 		}
-		barr_uninit(&notes);
+		barr_uninit(&nstrs);
 	}
 
 	for(i = 0; i < PBT_STRINGCNT; ++i) {
@@ -111,6 +183,11 @@ end_label:
 			buninit(&strings[i]);
 		}
 	}
+
+	if(tempstr != NULL)
+		buninit(&tempstr);
+	if(noteln != NULL)
+		buninit(&noteln);
 
 	return err?EINVAL:0;
 }
